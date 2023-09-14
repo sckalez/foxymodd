a3997935ad24f7c649939ee1ff34eb8a6ed38b2d
foxy <100508410+sckalez@users.noreply.github.com>
2023-09-14 18:50:06 -0400

    #5 added ruby ores. did not add recipes, loot tables or tags in hopes data generation will handle them

--- src/main/java/net/foxy/foxymodd/block/ModBlocks.java
+++ src/main/java/net/foxy/foxymodd/block/ModBlocks.java
@@ -5,12 +5,14 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.foxy.foxymodd.FoxyModd;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
@@ -18,6 +20,14 @@ public class ModBlocks {
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(2f), UniformIntProvider.create(2, 5)));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE).strength(4f), UniformIntProvider.create(2, 5)));
+    public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.NETHERRACK).strength(1.5f), UniformIntProvider.create(2, 5)));
+    public static final Block END_STONE_RUBY_ORE = registerBlock("end_stone_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.END_STONE).strength(4f), UniformIntProvider.create(4, 7)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -29,6 +39,6 @@ public class ModBlocks {
                 new BlockItem(block, new FabricItemSettings()));
     }
     public static void registerModBlocks() {
-        FoxyModd.LOGGER.info("Registering ModBlocks for " + FoxyModd.MOD_ID);
+        FoxyModd.LOGGER.info("Registering Mod Blocks for " + FoxyModd.MOD_ID);
     }
 }
